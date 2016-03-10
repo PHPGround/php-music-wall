1629f5804e9126bf5fff8bc6682c66ea212cfdcb
Lakshmi Kotteda <lakshmikotteda@hotmail.com>
2016-03-10 04:09:09 +0000

    Added song div placeholders

--- application/views/songs/index.php
+++ application/views/songs/index.php
@@ -1,3 +1,17 @@
 <h2 class="text-center"><?php echo $title; ?></h2>
 
 <h4 class="text-center">Song List</h4>
+
+<div class="container">
+  <div class="text-center song-div"> 
+    <p> Song Title </p> 
+    <p> Artist name </p>
+    <a href="#"> Link to song </a>
+  </div>
+
+  <div class="text-center song-div"> 
+    <p> Song Title 2</p> 
+    <p> Artist name 2</p>
+    <a href="#"> Link to song 2</a>
+  </div>  
+</div>
